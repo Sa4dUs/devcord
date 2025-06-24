caa855231bb5d5d35be12bf15e1353f6d521f918
Lamoara <jorginos40@gmail.com>
2025-06-24 17:14:00 +0200

    temp fix

--- backend/user-service/src/sql_utils.rs
+++ backend/user-service/src/sql_utils.rs
@@ -13,26 +13,6 @@ pub enum RequestState {
     Rejected,
 }
 
-pub async fn check_request_in_db(from: &str, to: &str, db: &sqlx::PgPool) -> RequestState {
-    let res = sqlx::query_as!(
-        String,
-        "SELECT status FROM friend_requests WHERE from_user_id = $1 AND to_user_id = $2"
-    )
-    .bind(from)
-    .bind(to)
-    .fetch_one(db)
-    .await
-    .ok();
-
-    match res {
-        None => RequestState::None,
-        Some(state) if state.into() == "pending" => RequestState::Pending,
-        Some(state) if state.into() == "accepted" => RequestState::Accepted,
-        Some(state) if state.into() == "rejected" => RequestState::Rejected,
-        _ => RequestState::None,
-    }
-}
-
 pub async fn create_request(from: &str, to: &str, db: &sqlx::PgPool) -> anyhow::Result<()> {
     sqlx::query("INSERT INTO friend_requests (from_user_id, to_user_id) VALUES ($1, $2)")
         .bind(from)
