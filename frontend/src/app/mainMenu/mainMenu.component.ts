93d5e5110bdb6d43ed2eede2ccb336602a2d5fa8
AlexGarciaPrada <114813960+AlexGarciaPrada@users.noreply.github.com>
2025-07-13 00:54:04 +0200

    Revert "MainMenu Init"
    This reverts commit 670316987b4ed9a31db654b64bd401b8ce1bee1b.

--- frontend/src/app/mainMenu/mainMenu.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './mainMenu.component.html'
-})
-export class MainMenuComponent {}
