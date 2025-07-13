71547deaa7e27e49221257db9bec3dda48cc60af
AlexGarciaPrada <114813960+AlexGarciaPrada@users.noreply.github.com>
2025-07-14 00:17:23 +0200

    Bubble container

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { MainMenuComponent } from "./mainMenu/mainMenu.component";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MainMenuComponent],
     templateUrl: "./app.html",
     styleUrl: "./app.scss",
 })
