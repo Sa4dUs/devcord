9846f0f9b5114415457c1bef4354419dc07798b2
Marcelo Domínguez <dmmarcelo27@gmail.com>
2025-07-30 15:56:03 +0200

    Hardcode init script to make @Lamoara's life easier

--- backend/group-service/src/config.rs
+++ backend/group-service/src/config.rs
@@ -16,5 +16,28 @@ pub async fn init_db() -> anyhow::Result<PgPool> {
         .connect(&db_url)
         .await?;
 
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS groups (
+    id UUID PRIMARY KEY,
+    owner_id UUID NOT NULL,
+    channel_id UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
+",
+    )
+    .execute(&pool)
+    .await?;
+
+    sqlx::query(
+        "CREATE TABLE IF NOT EXISTS group_members (
+    group_id UUID REFERENCES groups(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL,
+    PRIMARY KEY (group_id, user_id)
+);
+",
+    )
+    .execute(&pool)
+    .await?;
+
     Ok(pool)
 }
