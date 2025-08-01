976130b58ac093b92545f06e89c74167f89e0727
Alejandro García Prada <114813960+AlexGarciaPrada@users.noreply.github.com>
2025-08-01 12:58:45 +0200

    Para pensar

---
+++ frontend/tests/app.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from "@playwright/test";
+
+test("Angular default app shows some text", async ({ page }) => {
+    await page.goto("/");
+    await page.goto("/home");
+    await expect(page.locator("body")).not.toBeEmpty();
+});
