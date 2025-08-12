49e079f6a0e04fef01b4205f4a0ca8d3b3402c0b
fuihfuefuiewn <danielbonachelamartinez@gmail.com>
2025-08-12 16:41:15 +0200

    Bug
    Con el cambio de nombre se dejó eso sin cambiar, creo que todo el origen de los males universales era esa tontería.
    En fin....es lo que hay, me pongo a mirar que no haya otras cosas similares pero que sepáis que me queda 1 hora de batería y los enchufes de la casa están ocupados...

--- frontend/src/app/log-in/log-in.component.ts
+++ frontend/src/app/log-in/log-in.component.ts
@@ -6,7 +6,7 @@ import { toSignal } from "@angular/core/rxjs-interop";
 import { ErrorsHandling } from "../errors/errors";
 import { SERVER_ROUTE } from "../../environment/environment.secret";
 
-const context = "login";
+const context = "log-in";
 @Component({
     selector: "log-in",
     standalone: true,
